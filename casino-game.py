fef71cf55a58febdd7b80badb451de4f2c026b6a
Anja <anja.lavrencic@gmail.com>
2020-04-20 21:09:10 +0200

    correction

--- casino-game.py
+++ casino-game.py
@@ -1,10 +1,10 @@
 secret_number = 9
-guess = ""
 
 guess = int(input("Enter guess: "))
 while guess != secret_number:
     print("Try again")
     guess = int(input("Enter guess: "))
+
 
 print("You win! ")
 
